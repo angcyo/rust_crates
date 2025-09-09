14825540b6799d702bf05f95dcb53986d5176ab6
angcyo <angcyo@126.com>
2025-09-09 16:34:18 +0800

    + ild

--- rc_gcode/src/ild.rs
+++ rc_gcode/src/ild.rs
@@ -1,3 +1,8 @@
+use crate::split_path_contours;
+use image::{DynamicImage, GenericImageView, Pixel, Rgba};
+use lyon_algorithms::walk::{walk_along_path, RegularPattern, WalkerEvent};
+use std::u8;
+
 ///
 /// @author <a href="mailto: angcyo@126.com">angcyo</a>
 /// @date 2025/09/09
@@ -132,14 +137,123 @@ impl IldWriter {
     }
 }
 
+/// 将路径[Path]转换为ild字节数据
+pub fn path_to_ild_bytes(
+    path: &lyon_path::Path,
+    tolerance: f32,
+    interval: f32,
+    r: u8,
+    g: u8,
+    b: u8,
+) -> Vec<u8> {
+    let mut writer = IldWriter::default();
+    // 获取路径上的点
+    let mut points = vec![];
+
+    let path_vec = split_path_contours(path);
+    for path in path_vec {
+        let start = 0.0;
+        let mut pattern = RegularPattern {
+            callback: &mut |event: WalkerEvent| {
+                points.push((event.position.x, event.position.y));
+                true
+            },
+            interval,
+        };
+        walk_along_path(path.iter(), start, tolerance, &mut pattern);
+    }
+
+    //防止超范围
+    let count = points.len() as u16;
+    writer.writer_header(5, 1, 0, count);
+    for i in 0..count {
+        let point = points[i as usize];
+        writer.write_point_2d_index_rgb(&(point.0 as i16, point.1 as i16), r, g, b);
+    }
+    writer.bytes
+}
+
+/// 将图片转换为ild字节数据
+/// - [frame_count] 总帧数, 范围为 1 – 65535。对于调色板，此值应为 0。
+/// - [frame_index] 当前帧索引, 计数从第 0 帧开始。范围为 0 – 65534。
+/// - [gray_threshold] 灰度阈值, >这个值的像素, 视为白色255, 无数据
+/// - [alpha_threshold] 透明阈值, 透明通道<=这个值的像素, 视为白色255, 无数据
+pub fn image_to_ild_bytes(
+    img: &DynamicImage,
+    offset_x: i16,
+    offset_y: i16,
+    gray_threshold: u8,
+    alpha_threshold: u8,
+) -> Vec<u8> {
+    let mut writer = IldWriter::default();
+    write_image_to_ild_bytes(
+        &mut writer,
+        img,
+        1,
+        0,
+        offset_x,
+        offset_y,
+        gray_threshold,
+        alpha_threshold,
+    );
+    writer.bytes
+}
+
+/// 写入一帧数据到ild数据中
+pub fn write_image_to_ild_bytes<I>(
+    writer: &mut IldWriter,
+    img: &I,
+    frame_count: u16,
+    frame_index: u16,
+    offset_x: i16,
+    offset_y: i16,
+    gray_threshold: u8,
+    alpha_threshold: u8,
+) where
+    I: GenericImageView<Pixel = Rgba<u8>>,
+{
+    // 获取路径上的点
+    let mut points = vec![];
+    for (x, y, pixel) in img.pixels() {
+        let r = pixel[0];
+        let g = pixel[1];
+        let b = pixel[2];
+        let a = pixel[3];
+
+        //计算灰度值
+        let g = (0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32) as u8;
+        if g > gray_threshold || a <= alpha_threshold {
+            //白色, 无数据
+        } else {
+            points.push((x as i16, y as i16, r, g, b));
+        }
+    }
+
+    //防止超范围
+    let count = points.len() as u16;
+    writer.writer_header(5, frame_count, frame_index, count);
+    for i in 0..count {
+        let point = points[i as usize];
+        writer.write_point_2d_index_rgb(
+            &(point.0 + offset_x, point.1 + offset_y),
+            point.2,
+            point.3,
+            point.4,
+        );
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::ild::IldWriter;
-    use lyon_algorithms::walk::{walk_along_path, RegularPattern, WalkerEvent};
+    use crate::ild::{path_to_ild_bytes, write_image_to_ild_bytes, IldWriter};
+    use image::codecs::gif::GifDecoder;
+    use image::AnimationDecoder;
     use lyon_path::math::point;
     use lyon_path::Path;
     use rc_basis::files::save_bytes_to_file;
-    use rc_basis::test::get_test_output_file_path;
+    use rc_basis::test::{get_test_file_path, get_test_output_file_path};
+    use std::fs::File;
+    use std::io::BufReader;
 
     #[test]
     fn test_path_to_ild_bytes() {
@@ -149,33 +263,69 @@ mod tests {
         builder.end(false);
         let path = builder.build();
 
-        // 获取路径上的点
-        let mut points = vec![];
-
-        let start = 0.0;
-        let interval = 1.0;
-        let mut pattern = RegularPattern {
-            callback: &mut |event: WalkerEvent| {
-                points.push((event.position.x, event.position.y));
-                true
-            },
-            interval,
-        };
-        walk_along_path(path.iter(), start, 0.1, &mut pattern);
-        
         let r = 0xff;
         let g = 0x1F;
         let b = 0xFF;
-        let mut writer = IldWriter::default();
-        writer.writer_header(5, 1, 0, points.len() as u16);
-        for point in points.iter() {
-            writer.write_point_2d_index_rgb(&(point.0 as i16, point.1 as i16), r, g, b);
-        }
+        let bytes = path_to_ild_bytes(&path, 0.1, 1.0, r, g, b);
 
         let output = get_test_output_file_path("path_to_ild.ild");
-        save_bytes_to_file(output.as_str(), &writer.bytes).unwrap();
+        save_bytes_to_file(output.as_str(), &bytes).unwrap();
     }
 
     #[test]
-    fn test_image_to_ild_bytes() {}
+    fn test_image_to_ild_bytes() {
+        //image_to_ild_bytes
+        let count = 10 as u16;
+        for i in 0..count {
+            println!("{}", i)
+        }
+    }
+
+    #[test]
+    fn test_gif_to_ild_bytes() {
+        let gif = File::open(get_test_file_path("向上走的小人.gif")).unwrap();
+        let reader = BufReader::new(gif);
+        let decoder = GifDecoder::new(reader).unwrap();
+        let frames = decoder.into_frames();
+        let frames = frames.collect_frames().expect("error decoding gif");
+        let len = frames.len();
+        let mut frame_index = 0;
+
+        let mut writer = IldWriter::default();
+        frames.into_iter().for_each(|frame| {
+            if (frame_index < 1) {
+                write_image_to_ild_bytes(
+                    &mut writer,
+                    frame.buffer(),
+                    len as u16,
+                    frame_index,
+                    0,
+                    0,
+                    250,
+                    128,
+                );
+            }
+            //let frame = frame.unwrap();
+            /*println!(
+                "->{:?} {:?} {:?}",
+                frame.left(),
+                frame.top(),
+                frame.buffer().dimensions()
+            );
+            println!("delay->{:?}", frame.delay());
+
+            frame
+                .buffer()
+                .save(get_test_output_file_path(
+                    format!("gif_frame_{}.png", frame_index).as_str(),
+                ))
+                .unwrap();*/
+
+            frame_index += 1;
+        });
+        println!("共:{}帧", len);
+
+        let output = get_test_output_file_path("gif_to_ild.ild");
+        save_bytes_to_file(output.as_str(), &writer.bytes).unwrap();
+    }
 }
